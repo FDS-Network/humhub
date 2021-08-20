011d5c0246702d9f18ec0e4b659f42d43cac31bc
DESKTOP-LSPDP1F\Thinh <thinh.do@fdssoft.com>
2021-08-20 10:36:35 +0700

    [hide-image]hide image in news in humhub

--- protected/humhub/modules/post/widgets/views/wallEntry.php
+++ protected/humhub/modules/post/widgets/views/wallEntry.php
@@ -1,16 +1,24 @@
 <?php
+
 use humhub\modules\content\widgets\richtext\RichText;
 use humhub\modules\content\widgets\stream\WallStreamEntryOptions;
 use humhub\modules\post\models\Post;
 
 /* @var $post Post */
+/* @var $isNews bool */
 /* @var $renderOptions WallStreamEntryOptions */
 
 $isDetailView = $renderOptions->isViewContext(WallStreamEntryOptions::VIEW_CONTEXT_DETAIL);
 
 ?>
-<div data-ui-widget="post.Post" <?php if(!$isDetailView) : ?>data-state="collapsed"<?php endif; ?> data-ui-init id="post-content-<?= $post->id ?>">
-    <div data-ui-markdown <?php if(!$isDetailView) : ?>data-ui-show-more<?php endif; ?>>
-        <?= RichText::output($post->message, ['record' => $post]) ?>
+<div data-ui-widget="post.Post" <?php if (!$isDetailView) : ?>data-state="collapsed"<?php endif; ?> data-ui-init
+     id="post-content-<?= $post->id ?>">
+    <div data-ui-markdown <?php if (!$isDetailView) : ?>data-ui-show-more<?php endif; ?>>
+        <?= RichText::output($post->message, [
+            'record' => $post,
+            'pluginOptions' => [
+                'isNews' => $isNews
+            ]
+        ]) ?>
     </div>
 </div>
